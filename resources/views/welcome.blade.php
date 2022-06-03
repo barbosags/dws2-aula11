065cbdfed040be9840893d6e1a80f24115365e71
barbosags <barbosa.g@aluno.ifsp.edu.br>
2022-06-03 19:00:41 -0300

    aula 18

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.main')
+
 @section('title', 'HDC Events')
+
 @section('content')
 
-    <div class="col-md-12" id="search-container">
-        <h1>Busque um Evento</h1>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
         <form action="">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
         </form>
     </div>
-
     <div id="events-container" class="col-md-12">
         <h2>Próximos Eventos</h2>
         <p class="subtitle">Veja os eventos dos próximos dias</p>
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
-                <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                     <div class="card-body">
                         <p class="card-date">10/09/2020</p>
                         <h5 class="card-title">{{ $event->title }}</h5>
                         <p class="card-participants">X Participantes</p>
-                    <a href="/events/{{$event->$id}}" class="btn btn-primary">Saber mais</a>
+                        <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
                     </div>
                 </div>
             @endforeach
