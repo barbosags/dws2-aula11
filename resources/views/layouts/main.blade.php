cbc920dbe309c906830d628a07e60b20a97237af
barbosags <barbosa.g@aluno.ifsp.edu.br>
2022-06-10 21:29:38 -0300

    Adjustment
    Aula dia 10 junho

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-    <!-- css -->
-    <link rel="stylesheet" href="/css/styles.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="/css/styles.css">
     <!-- Fontes google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
